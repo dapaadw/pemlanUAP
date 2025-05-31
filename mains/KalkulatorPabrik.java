249408aa951206010b11aa9722d131f4d1379d7a
dapaadw <dwikaanargya@gmail.com>
2025-05-31 20:26:02 +0700

    Second Commit

--- mains/KalkulatorPabrik.java
+++ mains/KalkulatorPabrik.java
@@ -12,38 +12,27 @@ public class KalkulatorPabrik {
         System.out.println("DAFFA DWIKA ANARGYA");
         System.out.println("245150700111035");
         System.out.println("=============================================");
-
         // Donat dengan lubang (Torus)
         System.out.println("Donat dengan lubang");
         System.out.println("=============================================");
-
         System.out.print("Isikan Radius : ");
         double majorRadius = sc.nextDouble();
-
         System.out.print("Isikan radius : ");
         double minorRadius = sc.nextDouble();
-
         System.out.println("=============================================");
-
         Torus donatDenganLubang = new Torus(majorRadius, minorRadius);
         donatDenganLubang.printInfo();
-
         System.out.println("=============================================");
 
         // Donat tanpa lubang (Sphere)
         System.out.println("Donat tanpa lubang");
         System.out.println("=============================================");
-
         System.out.print("Isikan radius : ");
-        double radiusBola = sc.nextDouble();
-
+        double radius = sc.nextDouble();
         System.out.println("=============================================");
-
-        Sphere donatTanpaLubang = new Sphere(radiusBola);
+        Sphere donatTanpaLubang = new Sphere(radius);
         donatTanpaLubang.printInfo();
-
         System.out.println("=============================================");
-
         sc.close();
     }
 }
